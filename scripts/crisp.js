09d92cf0a973ba8fef1687198e5d279ad3280130
KinglyMinecon <88132731+KinglyMinecon@users.noreply.github.com>
2025-03-05 12:22:59 +0000

    Change crisp site id

--- scripts/crisp.js
+++ scripts/crisp.js
@@ -1,6 +1,6 @@
 if (typeof window !== "undefined") {
     window.$crisp = [];
-    window.CRISP_WEBSITE_ID = "process.env.CRISP_SITE_ID";
+    window.CRISP_WEBSITE_ID = "cd0c87b2-b361-44f3-b60b-1c85caa7bfe1";
   
     (function () {
       const d = document;
